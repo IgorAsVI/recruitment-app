aa94215e8e86a788e924ae1826fcbd87dbaf2a4f
Paulo Sergio <juniorvasco616@gmail.com>
2025-06-09 21:43:40 -0300

    ajuste na view Meu Perfil

--- src/app/features/candidate/candidate-routing.module.ts
+++ src/app/features/candidate/candidate-routing.module.ts
@@ -1,8 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CandidateComponent } from './candidate.component';
+import { ProfileComponent } from './profile/profile.component';
+import { ResumeUploadComponent } from './resume-upload/resume-upload.component';
+import { ApplicationsComponent } from './applications/applications.component';
+import { RecommendedJobsComponent } from './recommended-jobs/recommended-jobs.component';
 
-const routes: Routes = [{ path: '', component: CandidateComponent }];
+const routes: Routes = [
+  {
+    path: '',
+    component: CandidateComponent,
+    children: [
+      { path: 'profile', component: ProfileComponent },
+      { path: 'resume', component: ResumeUploadComponent },
+      { path: 'applications', component: ApplicationsComponent },
+      { path: 'recommended-jobs', component: RecommendedJobsComponent },
+      { path: '', redirectTo: 'profile', pathMatch: 'full' }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
